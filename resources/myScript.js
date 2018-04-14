9b0a9fbe9c3916296eae0e983e33af3852bdf77a
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-14 19:19:44 -0400

    Add files via upload

---
+++ resources/myScript.js
@@ -0,0 +1,33 @@
+
+/////////////////////////////////////////////////////////////////////////////////////Scroll Functions with the navigation
+
+$(".jsClickMore").click(function(){
+  $("html, body").animate({scrollTop: $(".jsScrollMore").offset().top}, 1000);
+});
+
+$(".jsClickHow").click(function(){
+  $("html, body").animate({scrollTop: $(".jsScrollHow").offset().top}, 1000);
+});
+
+$(".jsClickAdd").click(function(){
+  $("html, body").animate({scrollTop: $(".jsScrollAdd").offset().top}, 1000);
+});
+
+$(".jsClickContact").click(function(){
+  $("html, body").animate({scrollTop: $(".jsScrollContact").offset().top}, 1000);
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////Sticky Navigation
+
+var mn = $(".stickyNav");
+
+$(window).scroll(function(){
+  if( $(this).scrollTop() > 800 ){
+    mn.addClass(".rowScrolled");
+
+  } else{
+    mn.removeClass(".rowScrolled");
+  }
+});
